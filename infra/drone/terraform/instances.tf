d58e0210d9fddffccceee5472e9030747134de59
mmskv <max.mishuk2014@gmail.com>
2022-11-02 16:37:34 +0300

    infra fixes

--- infra/drone/terraform/instances.tf
+++ infra/drone/terraform/instances.tf
@@ -1,24 +1,6 @@
-resource "openstack_networking_floatingip_v2" "master-fip" {
-  pool = "miem"
-}
-
-resource "openstack_networking_floatingip_v2" "exec1-fip" {
-  pool = "miem"
-}
-
-resource "openstack_compute_floatingip_associate_v2" "master-fip" {
-  floating_ip = openstack_networking_floatingip_v2.master-fip.address
-  instance_id = openstack_compute_instance_v2.master.id
-}
-
-resource "openstack_compute_floatingip_associate_v2" "exec1-fip" {
-  floating_ip = openstack_networking_floatingip_v2.exec1-fip.address
-  instance_id = openstack_compute_instance_v2.exec1.id
-}
-
 resource "openstack_compute_instance_v2" "master" {
   name        = "master"
-  image_name  = "Arch Linux with Docker"
+  image_name  = "Arch Linux"
   flavor_name = "m2s.medium"
   key_pair    = "admins"
   network {
