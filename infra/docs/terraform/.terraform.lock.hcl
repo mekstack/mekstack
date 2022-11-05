44928cd957addcb6db78db0c83fd9128720e69cc
TheHussky <apkrechetov@miem.hse.ru>
2022-11-05 12:43:31 +0300

    Adapt docs to infra changes

---
+++ infra/docs/terraform/.terraform.lock.hcl
@@ -0,0 +1,10 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/terraform-provider-openstack/openstack" {
+  version     = "1.35.0"
+  constraints = "~> 1.35.0"
+  hashes = [
+    "h1:k1SCosvSICWAgRkswl83KtCycN7iP9asejWDDEQEtuk=",
+  ]
+}
--- infra/docs/terraform/dns.tf
+++ infra/docs/terraform/dns.tf
@@ -1,34 +1,25 @@
 resource "openstack_dns_zone_v2" "docs_zone" {
-  name        = "docs.corp."
+  name        = "docs.miem."
   email       = "19106@miem.hse.ru"
   description = "docs zone"
   ttl         = 6000
   type        = "PRIMARY"
 }
 
-resource "openstack_dns_recordset_v2" "vip" {
-  zone_id     = openstack_dns_zone_v2.docs_zone.id
-  name        = "docs.corp."
-  description = "Docs web frontend"
-  ttl         = 3000
-  type        = "A"
-  records     = [openstack_lb_loadbalancer_v2.lb.vip_address]
-}
-
 resource "openstack_dns_recordset_v2" "web-1" {
   zone_id     = openstack_dns_zone_v2.docs_zone.id
-  name        = "web-1.docs.corp."
+  name        = "web-1.docs.miem."
   description = "web-1 instance"
   ttl         = 3000
   type        = "A"
-  records     = [openstack_networking_floatingip_v2.web-1.address]
+  records     = [openstack_compute_instance_v2.web-1.access_ip_v4]
 }
 
 resource "openstack_dns_recordset_v2" "web-2" {
   zone_id     = openstack_dns_zone_v2.docs_zone.id
-  name        = "web-2.docs.corp."
+  name        = "web-2.docs.miem."
   description = "web-2 instance"
   ttl         = 3000
   type        = "A"
-  records     = [openstack_networking_floatingip_v2.web-2.address]
+  records     = [openstack_compute_instance_v2.web-2.access_ip_v4]
 }
