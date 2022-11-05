234373dfdeb72ef003660765df6a75f6184608e0
TheHussky <apkrechetov@miem.hse.ru>
2022-11-05 12:42:34 +0300

    Set up vault integration

--- infra/drone/terraform/secgroup.tf
+++ infra/drone/terraform/secgroup.tf
@@ -2,6 +2,16 @@ data "openstack_networking_secgroup_v2" "default" {
   name = "default"
 }
 
+resource "openstack_networking_secgroup_rule_v2" "ssh" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  port_range_min    = 22
+  port_range_max    = 22
+  protocol          = "tcp"
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = data.openstack_networking_secgroup_v2.default.id
+}
+
 resource "openstack_networking_secgroup_rule_v2" "http" {
   direction         = "ingress"
   ethertype         = "IPv4"
@@ -21,4 +31,13 @@ resource "openstack_networking_secgroup_rule_v2" "https" {
   remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = data.openstack_networking_secgroup_v2.default.id
 }
+
+resource "openstack_networking_secgroup_rule_v2" "icmp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "icmp"
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = data.openstack_networking_secgroup_v2.default.id
+}
+
 
