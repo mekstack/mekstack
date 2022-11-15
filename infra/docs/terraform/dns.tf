f876f1b96641a981c89653e766cacfe8b80ad9ad
mmskv <max.mishuk2014@gmail.com>
2022-11-16 02:50:59 +0300

    Refactor docs IaC
    Refactor ansible playbook
    Refactor terraform
    Use Ubuntu instead of Arch Linux

--- infra/docs/terraform/dns.tf
+++ infra/docs/terraform/dns.tf
@@ -1,34 +1,16 @@
-resource "openstack_dns_zone_v2" "docs_zone" {
+resource "openstack_dns_zone_v2" "zone" {
   name        = "docs.miem."
   email       = "19106@miem.hse.ru"
-  description = "docs zone"
+  description = "${local.project} zone"
   ttl         = 6000
   type        = "PRIMARY"
 }
 
-resource "openstack_dns_recordset_v2" "docs_internal" { 
-  zone_id     = openstack_dns_zone_v2.docs_zone.id
+resource "openstack_dns_recordset_v2" "vip" {
+  zone_id     = openstack_dns_zone_v2.zone.id
   name        = "docs.miem."
-  description = "docs endpoint"
+  description = "Docs VIP"
   ttl         = 3000
   type        = "A"
-  records     = [openstack_compute_instance_v2.web-1.access_ip_v4]
-}
-
-resource "openstack_dns_recordset_v2" "web-1" { 
-  zone_id     = openstack_dns_zone_v2.docs_zone.id
-  name        = "web-1.docs.miem."
-  description = "web-1 instance"
-  ttl         = 3000
-  type        = "A"
-  records     = [openstack_compute_instance_v2.web-1.access_ip_v4]
-}
-
-resource "openstack_dns_recordset_v2" "web-2" {
-  zone_id     = openstack_dns_zone_v2.docs_zone.id
-  name        = "web-2.docs.miem."
-  description = "web-2 instance"
-  ttl         = 3000
-  type        = "A"
-  records     = [openstack_compute_instance_v2.web-2.access_ip_v4]
+  records     = [openstack_networking_floatingip_v2.fip.address]
 }
