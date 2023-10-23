63643324af86208776d6ff3ef7fed8579c0d4afe
mmskv <max.mishuk2014@gmail.com>
2023-10-23 22:45:25 +0300

    feat: don't publicise non-autobuild images

--- infra/admin/images.tf
+++ infra/admin/images.tf
@@ -6,16 +6,7 @@ locals {
       min_disk_gb = 3
       min_ram_mb  = 512
       decompress  = false
-      properties = {
-        os_type = "linux"
-      }
-    },
-    {
-      name        = "Arch Linux"
-      url         = "https://geo.mirror.pkgbuild.com/images/latest/Arch-Linux-x86_64-cloudimg.qcow2"
-      min_disk_gb = 3
-      min_ram_mb  = 512
-      decompress  = false
+      visibility  = "private"
       properties = {
         os_type = "linux"
       }
@@ -26,6 +17,7 @@ locals {
       min_disk_gb = 3
       min_ram_mb  = 512
       decompress  = true
+      visibility  = "public"
       properties = {
         os_type   = "linux"
         os_distro = "fedora-coreos"
@@ -43,7 +35,7 @@ resource "openstack_images_image_v2" "image" {
   image_cache_path = "./image_cache"
   container_format = "bare"
   disk_format      = "qcow2"
-  visibility       = "public"
+  visibility       = each.value.visibility
   min_disk_gb      = each.value.min_disk_gb
   min_ram_mb       = each.value.min_ram_mb
   properties       = each.value.properties
