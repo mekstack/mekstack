2022888868b3be6ec42ca42116d938fb543a1458
Nikittua <nisergeev_1@edu.hse.ru>
2023-11-02 20:50:56 +0300

    feat(registry): add variables with resource names

--- infra/registry/providers.tf
+++ infra/registry/providers.tf
@@ -1,8 +1,38 @@
 terraform {
   required_providers {
     openstack = {
-      source = "terraform-provider-openstack/openstack"
+      source  = "terraform-provider-openstack/openstack"
       version = "1.52.1"
     }
   }
+}
+
+variable "instance_count" {
+  type    = number
+  default = 2
+}
+
+variable "image" {
+  type = string
+  default = "Ubuntu 22.04.3 LTS"
+}
+
+variable "flavor" {
+  type = string
+  default = "m2s.small"
+}
+
+variable "network" {
+  type = string
+  default = "network_1"
+}
+
+variable "subnet" {
+  type = string
+  default = "subnet_1"
+}
+
+variable "router" {
+  type = string
+  default = "router_1"
 }
