3f78db84abc03c074fbe0030138a5967808bce76
Nikittua <nisergeev_1@edu.hse.ru>
2023-11-04 17:08:59 +0300

    docs(registry): changed var names

--- infra/registry/module_config.tf
+++ infra/registry/module_config.tf
@@ -7,11 +7,11 @@ terraform {
   }
 }
 
+
 variable "instance_count" {
   type    = number
   default = 2
 }
-
 variable "image" {
   type = string
   default = "Mekstack Ubuntu 22.04.3 LTS"
@@ -24,15 +24,15 @@ variable "flavor" {
 
 variable "network" {
   type = string
-  default = "network_1"
+  default = "registry_network_1"
 }
 
 variable "subnet" {
   type = string
-  default = "subnet_1"
+  default = "registry_subnet_1"
 }
 
 variable "router" {
   type = string
-  default = "router_1"
+  default = "registry_router_1"
 }
