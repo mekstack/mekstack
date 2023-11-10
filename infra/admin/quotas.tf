4863702cde406847d4d6762b5c82a262fdd18c8c
mmskv <max.mishuk2014@gmail.com>
2023-11-10 21:24:04 +0300

    chore: bump admin quotas

--- infra/admin/quotas.tf
+++ infra/admin/quotas.tf
@@ -1,20 +1,20 @@
 resource "openstack_compute_quotaset_v2" "admin" {
   project_id           = data.openstack_identity_auth_scope_v3.current.project_id
   key_pairs            = 10
-  ram                  = 1000000
+  ram                  = 100000
   cores                = 300
   instances            = 20
-  server_groups        = 4
-  server_group_members = 8
+  server_groups        = 20
+  server_group_members = 80
 }
 
 resource "openstack_blockstorage_quotaset_v3" "admin" {
   project_id           = data.openstack_identity_auth_scope_v3.current.project_id
-  volumes              = 20
-  snapshots            = 20
+  volumes              = 30
+  snapshots            = 30
   gigabytes            = 10000
   per_volume_gigabytes = 1000
-  backups              = 20
+  backups              = 30
   backup_gigabytes     = 1000
   groups               = 100
 }
