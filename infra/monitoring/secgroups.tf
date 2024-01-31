9159dbe6f4584882f1b0a58b217c442bffb731a4
lloydik <artemglazyrin@mail.ru>
2024-01-31 14:44:35 +0300

    feat: Add prometheus scripts deploying

--- infra/monitoring/secgroups.tf
+++ infra/monitoring/secgroups.tf
@@ -18,3 +18,13 @@ resource "openstack_networking_secgroup_rule_v2" "icmp" {
   remote_ip_prefix  = "0.0.0.0/0"
   security_group_id = openstack_networking_secgroup_v2.secgroup.id
 }
+
+resource "openstack_networking_secgroup_rule_v2" "tcp" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 9090
+  port_range_max    = 9090
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.secgroup.id
+}
