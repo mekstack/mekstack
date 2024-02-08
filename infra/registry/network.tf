baa6d9d5fc4449ac2975cba7eaf8d23ebc6e52f5
Nikita Sergeev <nsergeev@rvision.pro>
2024-02-08 15:11:28 +0300

    refactor(tf): changed networking/fp parameters

--- infra/registry/network.tf
+++ infra/registry/network.tf
@@ -6,38 +6,35 @@ resource "openstack_networking_network_v2" "network" {
 resource "openstack_networking_subnet_v2" "subnet" {
   name       = var.name
   network_id = openstack_networking_network_v2.network.id
-  cidr       = "172.16.98.0/23"
+  cidr       = "10.0.0.0/24"
   ip_version = 4
 }
 
 resource "openstack_networking_router_v2" "router" {
   name                = var.name
-  admin_state_up      = true
-  external_network_id = "4d942526-390d-40a1-8bd3-934405fd4281"
+  external_network_id = var.public_network.id
 }
 
 resource "openstack_networking_router_interface_v2" "router_interface" {
-
   router_id = openstack_networking_router_v2.router.id
   subnet_id = openstack_networking_subnet_v2.subnet.id
 }
 
 resource "openstack_networking_router_route_v2" "router_route" {
+  depends_on = [openstack_networking_router_interface_v2.router_interface]
 
   router_id        = openstack_networking_router_v2.router.id
   destination_cidr = "172.18.217.0/24"
   next_hop         = "172.18.218.2"
 }
 
-
 resource "openstack_networking_floatingip_v2" "fip" {
-  count = var.instances
-  pool  = "public"
+  pool        = var.public_network.name
+  address     = "172.18.219.1"
+  description = "HTTP(S) Reverse proxy gateway"
 }
 
-resource "openstack_compute_floatingip_associate_v2" "fip" {
-  count = var.instances
-
-  floating_ip = element(openstack_networking_floatingip_v2.fip[*].address, count.index)
-  instance_id = openstack_compute_instance_v2.instance[count.index].id
+resource "openstack_networking_floatingip_associate_v2" "lb_fip" {
+  port_id     = openstack_lb_loadbalancer_v2.lb.vip_port_id
+  floating_ip = openstack_networking_floatingip_v2.fip.address
 }
