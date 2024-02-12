78888fce85ba6e90129111b93ae44d55914985d7
TheHussky <apkrechetov@miem.hse.ru>
2024-02-13 01:23:37 +0300

    feat(tf): move tfstate to s3

--- infra/k3s/main.tf
+++ infra/k3s/main.tf
@@ -13,6 +13,21 @@ module "k3s" {
 }
 
 terraform {
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    region = "ru-central1"
+    key    = "k3s.tfstate"
+    bucket = "mekstack-tfstate"
+
+    # Those are required because terraform validates against aws
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
+
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
--- infra/sneedaas/config.tf
+++ infra/sneedaas/config.tf
@@ -1,4 +1,19 @@
 terraform {
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    region = "ru-central1"
+    key    = "sneedaas.tfstate"
+    bucket = "mekstack-tfstate"
+
+    # Those are required because terraform validates against aws
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+  }
+
   required_providers {
     openstack = {
       source  = "terraform-provider-openstack/openstack"
